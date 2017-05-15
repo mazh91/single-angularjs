06d7687d315679fa7ac538e6431c2b789d0036a6
mazh91 <milad.azh@ryerson.ca>
2017-05-15 01:57:44 -0400

    complete angularjs code + html events

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,17 +1,32 @@
-(function () {
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('LunchCheck', [])
+  angular.module('LunchCheck', [])
 
-.controller('LunchCheckController', LunchCheckController);
+    .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject($scope);
+  LunchCheckController.$inject = ['$scope'];
 
-function LunchCheckController($scope) {
-  $scope.name = "Yaakov";
-  $scope.sayHello = function () {
-    return "Hello Coursera!";
+  function LunchCheckController($scope) {
+    //$scope.msg = "##Nothing here!";
+    $scope.doCheck = function() {
+      var list_str = $scope.lunch_list;
+      if (list_str)
+        var list = list_str.split(',');
+      else {
+        $scope.msg = "Please enter data first";
+        return;
+      }
+      var n = 0;
+      for (var i in list)
+        n++;
+      if (n <= 3)
+        $scope.msg = "Enjoy!";
+      else
+        $scope.msg = "Too much!";
+    };
+
   };
-});
+})
 
-})();
+();
